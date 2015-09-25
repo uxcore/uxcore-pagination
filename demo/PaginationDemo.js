43420525dac79ef5e20ffa66eeea24469642aac6
biangang.bg <biangang.bg@alibaba-inc.com>
2015-09-25 10:41:19 +0800

    fix showSizeChange bug

--- demo/PaginationDemo.js
+++ demo/PaginationDemo.js
@@ -6,13 +6,13 @@ export default class Demo extends React.Component {
 		return (
 			<div>
 				<p>基础分页:</p>
-				<Pagination onChange={onChange} total={50} />
+				<Pagination showSizeChanger={true} onChange={onChange} total={50} />
 				<p>更多分页:</p>
 				<Pagination onChange={onChange} total={500} />
 				<p>快速跳转到某一页:</p>
 				<Pagination showQuickJumper={true} onChange={onChange} total={500} />
 				<p>迷你版本:</p>
-				<Pagination className="mini" onChange={onChange} total={50} />
+				<Pagination className="mini" showQuickJumper={true} onChange={onChange} total={50} />
 				<p>简单地翻页:</p>
 				<Pagination simple onChange={onChange} total={50} />
 			</div>
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RcPagination from 'rc-pagination';
 import assign from 'object-assign';
+import Select from 'uxcore-select2';
 
 export default class Pagination extends RcPagination {
 	constructor(props){
@@ -10,5 +11,7 @@ export default class Pagination extends RcPagination {
 Pagination.displayName = 'uxcore-pagination';
 Pagination.propTypes = RcPagination.propTypes;
 Pagination.defaultProps = assign(RcPagination.defaultProps, {
-	prefixCls: 'kuma-pagination'
+	prefixCls: 'kuma-pagination',
+	selectPrefixCls: 'kuma-select',
+	selectComponentClass: Select
 });
