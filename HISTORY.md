61f4c628ca6d0855e0c725f6bae0d1b1189c52d8
光弘 <guanghong.wsj@alibaba-inc.com>
2015-10-11 11:45:25 +0800

    fork from rc-pagination

---
+++ HISTORY.md
@@ -0,0 +1,4 @@
+## 0.1.3
+
+`CHANGED` Forked from rc-pagination
+`CHNAGED` simple style changed
---
+++ src/Options.js
@@ -0,0 +1,104 @@
+const React = require('react');
+const KEYCODE = require('./KeyCode');
+
+class Options extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      current: props.current,
+      _current: props.current,
+    };
+
+    ['_handleChange', '_changeSize', '_go'].forEach((method) => this[method] = this[method].bind(this));
+  }
+  render() {
+    const props = this.props;
+    const state = this.state;
+    const prefixCls = `${props.rootPrefixCls}-options`;
+    const sizeOptions = props.sizeOptions;
+    const pageSize = props.pageSize;
+    const changeSize = props.changeSize;
+    const quickGo = props.quickGo;
+    const Select = props.selectComponentClass;
+    let changeSelect = null;
+    let goInput = null;
+
+    if (!(changeSize || quickGo)) {
+      return null;
+    }
+
+    if (changeSize && Select) {
+      const Option = Select.Option;
+      changeSelect = (
+        <Select
+          prefixCls={props.selectPrefixCls} showSearch={false}
+          className={`${prefixCls}-size-changer`}
+          optionLabelProp="children"
+          dropdownClassName={`${prefixCls}-size-changer-dropdown`}
+          defaultValue={sizeOptions.indexOf(pageSize) == -1 ? sizeOptions[0]+"" : pageSize+"" } 
+          onChange={this._changeSize}>
+          {sizeOptions.map((option, index) => {
+            return <Option key={option} value={option + ""}>{option + "条/页"}</Option>
+          })}
+       </Select>
+      );
+    }
+
+    if (quickGo) {
+      goInput = (
+        <div title="Quick jump to page" className={`${prefixCls}-quick-jumper`}>
+          跳至
+          <input type="text" value={state._current} onChange={this._handleChange.bind(this)} onKeyUp={this._go.bind(this)}/>
+          页
+        </div>
+      );
+    }
+
+    return (
+      <div className={`${prefixCls}`}>
+        {changeSelect}
+        {goInput}
+      </div>
+    );
+  }
+
+  _changeSize(value) {
+    this.props.changeSize(Number(value));
+  }
+
+  _handleChange(evt) {
+    const _val = evt.target.value;
+
+    this.setState({
+      _current: _val,
+    });
+  }
+
+  _go(e) {
+    const _val = e.target.value;
+    if (_val === '') {
+      return;
+    }
+    let val = Number(this.state._current);
+    if (isNaN(val)) {
+      val = this.state.current;
+    }
+    if (e.keyCode === KEYCODE.ENTER) {
+      const c = this.props.quickGo(val);
+      this.setState({
+        _current: c,
+        current: c,
+      });
+    }
+  }
+}
+
+Options.propTypes = {
+  changeSize: React.PropTypes.func,
+  quickGo: React.PropTypes.func,
+  selectComponentClass: React.PropTypes.func,
+  current: React.PropTypes.number,
+};
+
+module.exports = Options;
